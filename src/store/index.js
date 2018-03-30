d9e2f8aafed39c02382a9552ea5e49454d58c714
protoEvangelion <ryantgarant@gmail.com>
2018-03-29 22:40:10 -0700

    Fix window variable for gatsby production build process

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
 import { createStore as createReduxStore } from 'redux'
 import rootReducer from './reducers'
 
-const createStore = () =>
-	createReduxStore(
-		rootReducer,
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+const createStore = () => {
+	if (typeof window !== 'undefined') {
+		return createReduxStore(
+			rootReducer,
+			window.__REDUX_DEVTOOLS_EXTENSION__ &&
+				window.__REDUX_DEVTOOLS_EXTENSION__()
+		)
+	}
+	return createReduxStore(rootReducer)
+}
 
 export default createStore
