e02d0fa104393e3b3cab518f2e07efcb666e5c16
protoEvangelion <ryantgarant@gmail.com>
2018-04-07 12:57:58 -0700

    Remove superfluous folder

--- atoms/LogoImage/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@kadira/storybook'
-import LogoImage from '.'
-
-storiesOf('LogoImage', module)
-  .add('default', () => (
-    <LogoImage width={200} />
-  ))
--- molecules/Post/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@kadira/storybook'
-import { Post } from 'components'
-
-storiesOf('Post', module)
-  .add('default', () => (
-    <Post title="Hello" body="Ullamco et reprehenderit magna cillum ullamco consectetur et enim aliqua." />
-  ))
