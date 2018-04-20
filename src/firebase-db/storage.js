2a296a401af72aa4361c182c7100fd3c72411b4b
protoEvangelion <ryantgarant@gmail.com>
2018-04-20 00:32:16 -0700

    Wrap uploadBooths function in isApprovedUser call

--- src/firebase-db/storage.js
+++ src/firebase-db/storage.js
@@ -1,26 +1,29 @@
 import { db, storage } from './'
+import { isApprovedUser } from './auth'
 
 const uploadBooths = (snapshot, location) => {
-	const booths = snapshot.val()
+	if (isApprovedUser()) {
+		const booths = snapshot.val()
 
-	const today = new Date().toISOString().slice(0, 10)
+		const today = new Date().toISOString().slice(0, 10)
 
-	const file = `${today}__${location}.json`
+		const file = `${today}__${location}.json`
 
-	const fileRef = storage.ref().child(file)
+		const fileRef = storage.ref().child(file)
 
-	fileRef.getDownloadURL().then(
-		() => {
-			// Backup already exists so do nothing
-		},
-		() => {
-			// File does not exist so proceed with backup
-			fileRef
-				.putString(JSON.stringify(booths))
-				.then(() => console.log('Successfully backed up booths for the day!'))
-				.catch(err => console.log('Error backing up booths', err))
-		}
-	)
+		fileRef.getDownloadURL().then(
+			() => {
+				// Backup already exists so do nothing
+			},
+			() => {
+				// File does not exist so proceed with backup
+				fileRef
+					.putString(JSON.stringify(booths))
+					.then(() => console.log('Successfully backed up booths for the day!'))
+					.catch(err => console.log('Error backing up booths', err))
+			}
+		)
+	}
 }
 
 export const backupBooths = () => {
